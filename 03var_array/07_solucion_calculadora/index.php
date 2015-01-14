8402a19c77187d87d3320f0efb8f8845f3f8bcb5
mjosemargar <sennoramaria@gmail.com>
2015-01-14 12:13:37 +0100

    Añado solucion

---
+++ 03var_array/07_solucion_calculadora/index.php
@@ -0,0 +1,21 @@
+<?php include ('header.php');?> 
+	<div class='container'>
+		<form method='post' action='procesar.php'>
+			<label for='numero1' > Número 1 </label>
+			<input name='numero1' type="text" placeholder="Escribe un número" required>
+			<label> Número 2
+			<input name='numero2' type='text' placeholder="Escribe un número" required>
+			</label>
+			<label for='operacion'> Operación </label>
+			<select name='operacion'>
+				<option value='suma'> Sumar </option>
+				<option value='restar'> Restar </option>
+				<option value='multiplicar'> Multiplicar </option>
+				<option value='dividir'> Dividir </option>
+				<option value='modulo'> Módulo </option>
+			</select>
+			<br>
+			<button> Enviar </button>
+		</form>
+		</div>
+<?php include 'footer.php';?>
---
+++ 03var_array/07_solucion_calculadora/procesar.php
@@ -0,0 +1,26 @@
+<?php  
+	$numero1=$_POST['numero1'];
+	$numero2=$_POST['numero2'];
+	$operacion=$_POST['operacion'];
+	if ($operacion=="suma") 
+	{
+		$resultado=$numero1+$numero2;
+	}
+	if ($operacion=="restar")
+	{
+		$resultado=$numero1-$numero2;
+	}
+	if ($operacion=="multiplicar")
+	{
+		$resultado=$numero1*$numero2;
+	}
+	if ($operacion=="dividir")
+	{
+		$resultado=$numero1/$numero2;
+	}
+	if ($operacion=="modulo")
+	{
+		$resultado=$numero1%$numero2;
+	}
+	echo $resultado;
+?>
